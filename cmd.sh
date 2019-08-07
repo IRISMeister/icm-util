172211d53d4fa7f4e7532f04a995aae2a4123023
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2019-08-07 15:05:52 +0900

    ignore cmd.sh

--- cmd.sh
+++
@@ -1,2 +0,0 @@
-docker exec myicm sh -c "ssh -i /Samples/ssh/insecure -oStrictHostKeyChecking=no ubuntu@52.185.185.122 ip -4 -br a show dev eth0 | awk '{ print \$3}' | awk -F'/' '{ print \$1 }'"
-docker exec myicm sh -c "ssh -i /Samples/ssh/insecure -oStrictHostKeyChecking=no ubuntu@52.185.185.121 ip -4 -br a show dev eth0 | awk '{ print \$3}' | awk -F'/' '{ print \$1 }'"
