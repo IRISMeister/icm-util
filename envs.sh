ecfce597e3988b0c45d923eef3dc93aa4ce3461f
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2020-07-31 10:14:22 +0900

    2020.1

--- envs.sh
+++ envs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-export icmimg=intersystems/icm:2019.4.0.383.0
+export icmimg=intersystems/icm:2020.1.0.215.0
+#export icmimg=intersystems/icm:2019.4.0.383.0
 #export icmimg=intersystems/icm:2019.3.0.311.0
 #export icmimg=intersystems/icm:2019.2.0.109.0
 #export icmimg=intersystems/icm:2019.1.1.612.0
 
-export kitname=IRIS-2019.4.0.382.0-lnxubuntux64.tar.gz
+export kitname=IRIS-2020.1.0.215.0-lnxubuntux64.tar.gz
+#export kitname=IRIS-2019.4.0.382.0-lnxubuntux64.tar.gz
 #export kitname=IRIS-2019.4.0.382.0-lnxrhx64.tar.gz
 #export kitname=IRIS-2019.3.0.310.0-lnxrhx64.tar.gz
 #export kitname=IRIS-2019.1.1.612.0-lnxrhx64.tar.gz
@@ -24,12 +26,13 @@ export defaults=defaults.json
 #export defaults=defaults-container.json
 #export defaults=defaults-mirror.json
 
+export definitions=definitions-bh.json
 #export definitions=definitions.json
 #export definitions=definitions-mirror.json
 #export definitions=definitions-shard.json
-export definitions=definitions-shard-node.json
-#export definitions=definitions-standalone.json
+#export definitions=definitions-shard-node.json
 
 export icmdata=/Production/$provider
 
-export cpffile=UserCPF/merge.cpf
+#export cpffile=UserCPF/merge.cpf
+export cpffile=UserCPF/merge-min.cpf
