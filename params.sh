0b7add6f28282636d4afbf6f40d06b97f925c657
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2020-08-14 12:18:04 +0900

    renamed to params.sh because it is what it is

--- params.sh
+++ params.sh
@@ -12,8 +12,8 @@ export kitname=IRIS-2020.1.0.215.0-lnxubuntux64.tar.gz
 #export kitname=IRIS-2019.3.0.310.0-lnxrhx64.tar.gz
 #export kitname=IRIS-2019.1.1.612.0-lnxrhx64.tar.gz
 
-#export provider=azure
-export provider=aws
+export provider=azure
+#export provider=aws
 
 #export targetos=centos
 #export targetos=redhat
@@ -28,11 +28,23 @@ export defaults=defaults.json
 
 export definitions=definitions.json
 #export definitions=definitions-bh.json
+#export definitions=definitions-bh-mirror.json
 #export definitions=definitions-mirror.json
 #export definitions=definitions-shard.json
 #export definitions=definitions-shard-node.json
 
+#export cpffile=UserCPF/merge.cpf
+export cpffile=UserCPF/merge-min.cpf
+
+export reuseExistingEnv=false
+
+# ------------------------------
+# do not touch below lines
 export icmdata=/Production/$provider
 
-export cpffile=UserCPF/merge.cpf
-#export cpffile=UserCPF/merge-min.cpf
+export defaultspath=$defaultsroot/$provider/$targetos
+#use Label as a container name
+export icmname=$(cat $defaultspath/$defaults | jq -r '.Label')
+# Is this Containerless or not?
+export isContainerless=$(cat $defaultspath/$defaults | jq -r '.Containerless')
+
